ac9266690bf42500e4e5988c632e82d9fc01eb3b
RobertoSimonini <robertosimonini1@gmail.com>
2023-01-26 17:03:44 +0100

    added series

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,10 @@ export default {
   data (){
     return {
       store,
-      searchMovie: '',
+      searchProduct: '',
       apiUri: 'https://api.themoviedb.org/3/search/movie?api_key=a98e974a1dcb89c6e968d61b061b2de8&query=all',
-      movies: []
+      movies: [],
+      series: []
       
     }
   },
@@ -19,14 +20,25 @@ export default {
       });
       },
 
+      fetchSeries(url) {
+        axios.get(url)
+        .then((res) => {
+        this.series = res.data.results;
+      });
+      },
+
       onTypeSearch(){
-        const apiUri = `https://api.themoviedb.org/3/search/movie?api_key=a98e974a1dcb89c6e968d61b061b2de8&query=${this.searchMovie}`
-        this.fetchMovie(apiUri)
+        const movieUri = `https://api.themoviedb.org/3/search/movie?api_key=a98e974a1dcb89c6e968d61b061b2de8&query=${this.searchMovie}`
+        this.fetchMovie(movieUri);
+
+        const seriesUri = `https://api.themoviedb.org/3/search/tv?api_key=a98e974a1dcb89c6e968d61b061b2de8&query=${this.searchMovie}`
+        this.fetchSeries(seriesUri)
       },
 
       cleanInput(){
         if (this.searchMovie === ''){
           this.movies = []
+          this.series = []
         }
       },
   },
@@ -45,21 +57,42 @@ export default {
     <input @keyup="cleanInput" @keyup.enter="onTypeSearch" v-model.trim="searchMovie" type="text" class="form-control" placeholder="Search a Movie" aria-label="Username" aria-describedby="addon-wrapping">
     <button @click="onTypeSearch" class="btn btn-success">Search</button>
   </div>
+    <div class="movies">
+      <h1>Movies</h1>
+      <ul> 
+        <li v-for="movie in movies">
+          <div>
+              {{ movie.title }}        
+          </div>
+          <div>
+              {{ movie.original_title }}        
+          </div>
+          <img class="img-fluid flag" :src="`/src/assets/img/${movie.original_language}.png`">  
+          <div>
+              {{ movie.vote_average }}        
+          </div>
+        </li>
+      </ul>
+    </div>
 
-  <ul> 
-    <li v-for="movie in movies">
-      <div>
-          {{ movie.title }}        
-      </div>
-      <div>
-          {{ movie.original_title }}        
-      </div>
-      <img class="img-fluid flag" :src="`/src/assets/img/${movie.original_language}.png`">  
-      <div>
-          {{ movie.vote_average }}        
-      </div>
-    </li>
-  </ul>
+    <div class="series">
+      <h1>Series</h1>
+      <ul>
+        <li v-for="serie in series">
+          <div>
+              {{ serie.name }}        
+          </div>
+          <div>
+              {{ serie.original_name }}        
+          </div>
+          <img class="img-fluid flag" :src="`/src/assets/img/${serie.original_language}.png`">  
+          <div>
+              {{ serie.vote_average }}        
+          </div>
+        </li>
+      </ul>
+    </div>
+
 
 </template>
 
@@ -67,5 +100,6 @@ export default {
 <style> 
   .flag {
     height: 25px;
+    width: 35px;
   }
 </style>
