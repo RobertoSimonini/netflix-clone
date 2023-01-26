6e37f032ecb961e939cf2ebd373d6d586d359ffd
RobertoSimonini <robertosimonini1@gmail.com>
2023-01-26 12:55:20 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<script>
+import axios from 'axios'
+export default {
+
+}
+</script>
+
+
+
+
+<template>
+  <h1 class="text-danger">
+    Hello Boolflix
+  </h1>
+</template>
+
+
+
+
+<style>
+
+</style>
---
+++ src/main.js
@@ -0,0 +1,6 @@
+import { createApp } from 'vue'
+import './style.css'
+import App from './App.vue'
+import Bootstrap from 'bootstrap/dist/css/bootstrap.min.css'
+
+createApp(App).mount('#app')
