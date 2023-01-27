d86dcab4e65adddd7febc85f35a3066b8ec1d8a0
RobertoSimonini <robertosimonini1@gmail.com>
2023-01-27 11:20:19 +0100

    Rounded for excess the votes

--- src/App.vue
+++ src/App.vue
@@ -41,6 +41,7 @@ export default {
         if (searchProduct === ''){
           store.movies = []
           store.series = []
+          return
         }
       }
   },
@@ -52,50 +53,10 @@ export default {
 
 <template>
  <app-header @product-search="onProductSearch" @clean-input="cleanInput"></app-header>
+ <app-main></app-main>
 
   
-    <div class="movies">
-      <h1>Movies:</h1>
-      <ul> 
-        <li v-for="movie in store.movies">
-          <figure>
-            <img :src="`https://image.tmdb.org/t/p/w342/${movie.poster_path}`" :alt="movie.title">
-          </figure>
-          <div>
-              {{ movie.title }}        
-          </div>
-          <div>
-              {{ movie.original_title }}        
-          </div>
-          <img class="img-fluid flag" :src="`/src/assets/img/${movie.original_language}.png`">  
-          <div>
-              {{ movie.vote_average }}        
-          </div>
-        </li>
-      </ul>
-    </div>
-
-    <div class="series">
-      <h1>Series:</h1>
-      <ul>
-        <li v-for="serie in store.series">
-          <figure>
-            <img :src="`https://image.tmdb.org/t/p/w342/${serie.poster_path}`" :alt="serie.name">
-          </figure>
-          <div>
-              {{ serie.name }}        
-          </div>
-          <div>
-              {{ serie.original_name }}        
-          </div>
-          <img class="img-fluid flag" :src="`/src/assets/img/${serie.original_language}.png`">
-          <div>
-              {{ serie.vote_average }}        
-          </div>
-        </li>
-      </ul>
-    </div>
-
+    
 
 </template>
 
